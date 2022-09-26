8d372a0223028552494d4b3cc400cccdcb589d33
Denis Elezi <deniselezi2003@outlook.com>
2022-09-26 05:46:02 +0100

    added some styling, started pulling tags

--- index.js
+++ index.js
@@ -15,11 +15,33 @@ function updateCountdown() {
   const currentTime = Date.now();
   const timeRemaining = 30 - ((currentTime - loadingTime) / 1000);
   if (timeRemaining > 0) {
-    document.getElementById("countdown").innerHTML = `get a new cat in: ${Math.round(timeRemaining)}`;
+    document.getElementById("countdown").innerText = `get a new cat in: ${Math.round(timeRemaining)}`;
   } else {
-    document.getElementById("countdown").innerHTML = "click the button now!";
+    document.getElementById("countdown").innerText = "click the button now!";
     document.getElementById("random-cat-button").disabled = false;
   }
 }
 
 setInterval(updateCountdown, 1000)
+
+async function getTags() {
+  var tags = await axios.get("https://cataas.com/api/tags");
+  tags = tags.data;
+  // in future, replace tags.length with tags-per-page limit
+  for (let i = 0 ; i < tags.length ; i++) {
+    // createTag(tags[i]);
+  }
+}
+
+// in future, change the place where the tag is appended
+function createTag(text) {
+  const tagsList = document.getElementById("tags-list");
+  const newLi = document.createElement("li");
+  const newTagText = document.createTextNode(text);
+  newTag = document.createElement("a")
+  newTag.appendChild(newTagText);
+  newLi.appendChild(newTag);
+  tagsList.appendChild(newLi);
+}
+
+getTags();
